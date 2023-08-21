ac46288a848e22d8cdc87b7f729e9f713acc7928
Sohyun kim <rlathgus541@naver.com>
2023-08-21 17:15:45 +0900

    입력 값 추가:POST, JSON.stringify

--- post/index.js
+++ post/index.js
@@ -19,7 +19,7 @@ function cardTemplate(writing) {
   return template;
 }
 
-// 로딩
+// 로딩-> display
 (async () => {
   const url = "http://localhost:8080/posts";
   // 1. 서버에서 데이터 가져옴(data:jason 형식(텍스트))
@@ -29,12 +29,56 @@ function cardTemplate(writing) {
   const result = await response.jason();
   console(result);
 
-  // 배열 형태
+  // 배열 형태 -> forEach()
   const data = Array.from(result);
   console.log(data);
 
-  // 2. 데이터배열 반복문으로 html문자열 추가
+  // 2. 데이터배열에 html문자열 추가-> 현재 폼 바로 다음에 display(동적).
   data.forEach((writing) => {
     document.forms[0].insertAdjacentHTML("afterend", cardTemplate(writing));
+  });
+})();
+
+// 작성된 포스팅 추가 기능
+(() => {
+  //컬렉션
+  const form = document.forms[0];
+  const post = form.querySelector("btn-submit");
+
+  const type = form.querySelectorAll("menu-type");
+  const title = form.querySelector("title");
+  const menu = form.querySelector("menu");
+  const review = form.querySelector("textarea");
+
+  post.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    //
+    const response = await fetch("http://localhost:8080/posts", {
+      // request: 추가
+      method: "POST",
+      headers: {
+        // 요청의 본문이 JSON 형식임을 서버에 알려줌.
+        "content -type": "application/jason",
+      },
+      // 요청의 본문에 실제 데이터 담음
+      body: JSON.stringify({
+        // JavaScript 객체를 JSON 문자열로 변환한 후 전송
+        type: type.values,
+        title: title.value,
+        menu: menu.value,
+        review: review.value,
+      }),
+    });
+
+    console.log(response);
+    const request = await response.jason();
+    console.log(result);
+
+    // UI 추가 (display)
+    document.forms[0].insertAdjacentHTML(
+      "afterend",
+      cardTemplate(request.data)
+    );
   });
 })();
