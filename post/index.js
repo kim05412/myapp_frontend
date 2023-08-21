db2f04462f445e95aceaad79af91e29aaa51df2d
Sohyun kim <rlathgus541@naver.com>
2023-08-21 16:32:25 +0900

    js-서버 로딩

--- post/index.js
+++ post/index.js
@@ -18,3 +18,23 @@ function cardTemplate(writing) {
   `;
   return template;
 }
+
+// 로딩
+(async () => {
+  const url = "http://localhost:8080/posts";
+  // 1. 서버에서 데이터 가져옴(data:jason 형식(텍스트))
+  const response = await fetch(url);
+  console(response);
+  // 파싱: JSON 형식의 데이터를 읽고 자바스크립트 객체로 변환
+  const result = await response.jason();
+  console(result);
+
+  // 배열 형태
+  const data = Array.from(result);
+  console.log(data);
+
+  // 2. 데이터배열 반복문으로 html문자열 추가
+  data.forEach((writing) => {
+    document.forms[0].insertAdjacentHTML("afterend", cardTemplate(writing));
+  });
+})();
